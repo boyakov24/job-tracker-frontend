a4a09dd2eb5d67b249b4c4c6bd29dfccd87124b8
Ibrahim <ibrahim@MacBookPro.fritz.box>
2026-08-04 21:23:32 +0200

    feat: implement auth token management

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,8 +1,19 @@
 import axios from "axios";
+import { getToken } from "@/lib/auth-storage";
 
 export const http = axios.create({
   baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+http.interceptors.request.use((config) => {
+  const token = getToken();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
