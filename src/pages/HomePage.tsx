e7a05047a2320dab3338acbb1a1d44b36ec93b7e
Ibrahim <ibrahim@MacBook-Pro.fritz.box>
2026-08-04 08:01:52 +0200

    chore: install tailwind and shadcn ui

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-100">
+      <Button>Job Tracker</Button>
+    </div>
+  );
 }
 
 export default HomePage;
